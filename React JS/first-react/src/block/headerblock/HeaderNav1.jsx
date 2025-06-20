0e71ec3da417f75f4bc10dc2aba9e178701162ff
Sammy <olorundasamuel2@gmail.com>
2025-06-20 12:28:04 +0100

    react js

---
+++ React JS/first-react/src/block/headerblock/HeaderNav1.jsx
@@ -0,0 +1,13 @@
+import "./HeaderNav.css";
+const HeaderNav1 = () => {
+  return (
+    <div className="headerNav1Container">
+      <img src="" alt="Logo" />
+      <nav>Home</nav>
+      <nav>About</nav>
+      <nav>Contact</nav>
+    </div>
+  );
+};
+
+export default HeaderNav1;
---
+++ React JS/first-react/src/block/heroblock/HeroImage.jsx
@@ -0,0 +1,9 @@
+const HeroImage = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default HeroImage;
